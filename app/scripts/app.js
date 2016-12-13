ec9b83bb7e1f9e6d3de0dd8832d386bc34f62385
Alpha SALL <alsall@free.fr>
2016-12-13 10:42:39 -0500

    Project scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularTopSongsApp
+ * @description
+ * # angularTopSongsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularTopSongsApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router',
+    'restangular'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTopSongsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularTopSongsApp
+ */
+angular.module('angularTopSongsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
